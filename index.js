37affa6002306b1a90377da1f662a6beacb44583
Silasoli <silas.oliveira@blockhub.builders>
2023-06-10 00:59:31 -0300

    feat: adjust logic

--- index.js
+++ index.js
@@ -5,35 +5,74 @@ async function getPageContent() {
   const page = await browser.newPage();
   await page.goto("https://tradingview.com/symbols/EURUSD/technicals/");
 
+  const findM10 = "Exponential Moving Average (10)";
+  const findM20 = "Exponential Moving Average (20)";
+  const currentValue = await getCurrentValue(page);
+
+  const table = await getTable(page);
+
+  await browser.close();
+
+  const items = parseTableData(table);
+  const M10 = getItemByName(findM10, items);
+  const M20 = getItemByName(findM20, items);
+
+  return decideAction(M10, M20, currentValue);
+}
+
+async function decideAction(M10, M20, currentValue) {
+  console.log(`Valor atual: ${currentValue}`);
+  console.log(`${M10.name}: ${M10.value}`);
+  console.log(`${M20.name}: ${M20.value}`);
+
+  if (currentValue < M10.value && currentValue <= M20.value) {
+    return "Comprar";
+  }
+
+  if (currentValue >= M20.value && currentValue > M10.value) {
+    return "Vender";
+  }
+
+  return null;
+}
+
+function getItemByName(name, items) {
+  return items.find((item) => item.name === name);
+}
+
+async function getTable(page) {
   await page.waitForSelector(
     'button[role="tab"][aria-selected="false"][id="1m"]'
   );
 
   await page.click('button[role="tab"][aria-selected="false"][id="1m"]');
-
   await new Promise((resolve) => setTimeout(resolve, 1000));
 
   const elements = await page.$$(".tableWrapper-hvDpy38G");
-
   const elementTable = await page.evaluate(
     (element) => element.querySelector("table").textContent,
     elements[1]
   );
 
-  await browser.close();
-
   return elementTable;
+}
+
+async function getCurrentValue(page) {
+  const elements = await page.$$(".lastContainer-JWoJqCpY");
+  const elementTable = await page.evaluate(
+    (elem) => elem.querySelector("span").textContent,
+    elements[0]
+  );
+  return elementTable.slice(0, -1);
 }
 
 function removeWordsAtStart(string) {
   const wordsAtStart = ["Sell", "Buy", "Neutral"];
-
   for (let word of wordsAtStart) {
     if (string.startsWith(word)) {
       string = string.slice(word.length).trim();
     }
   }
-
   return string;
 }
 
@@ -41,8 +80,8 @@ function parseTableData(elementTable) {
   const prefix = "NameValueAction";
   elementTable = elementTable.substring(prefix.length);
 
-  const pattern = /([A-Z][\w\s]+?) \(\d+\)(\d\.\d{5})(Sell|Buy|Neutral|—)/g;
-  const objects = [...elementTable.matchAll(pattern)].map((match) => {
+  const pattern = /([A-Z][\w\s]+? \(\d+\))(\d\.\d{5})(Sell|Buy|Neutral|—)/g;
+  const objects = Array.from(elementTable.matchAll(pattern), (match) => {
     const name = removeWordsAtStart(match[1].trim());
     const value = match[2];
     const action = match[3];
@@ -53,17 +92,11 @@ function parseTableData(elementTable) {
 }
 
 async function getData() {
-  console.log(new Date())
-  const elementTable = await getPageContent();
-  const objects = parseTableData(elementTable);
-
-  for (const obj of objects) {
-    console.log(obj);
+  while (true) {
+    const result = await getPageContent();
+    console.log(result);
+    await new Promise((r) => setTimeout(r, 60000 * 2));
   }
 }
 
-// Chama a função getData inicialmente
 getData();
-
-// Executa a função getData a cada 10 minutos
-setInterval(getData, 10 * 60 * 1000);
